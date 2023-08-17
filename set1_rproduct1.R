80ec4a692cb685d43a175d189862f184b4359309
Jenna Jordan <jbjordan@uncg.edu>
2023-08-17 10:24:05 -0400

    Change from a to v

---
+++ set1_rproduct1.R
@@ -0,0 +1,14 @@
+# sample script -----------------------------------------------------------
+
+#calculate mean
+a <- c(1, 4, 8)
+mu_a <- mean(a)
+
+print (mu_a)
+
+# another sample script ---------------------------------------------------
+
+v <- c(1, 4, 8)
+med_v <- median(v)
+print (med_v)
+
